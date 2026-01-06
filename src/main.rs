9c42defdaaf522600d22de4439ebf2388d869140
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2026-01-06 19:52:51 +0000

    normal vs insert mode

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,74 @@
-use std::io::{stdin, stdout, Result, Error, Read, Write};
-use ratatui::termion::raw::IntoRawMode;
-use ratatui::termion::input::TermRead;
 use ratatui::termion::event::Key;
+use ratatui::termion::input::TermRead;
+use ratatui::termion::raw::IntoRawMode;
 use ratatui::termion::terminal_size;
 use std::env;
+use std::io::{Error, Read, Result, Write, stdin, stdout};
+
+enum Mode {
+    Normal,
+    Insert,
+}
 
 struct Editor {
+    mode: Mode,
 }
 
 impl Editor {
-    fn default() -> Self {Editor{}}
-    fn run(&self) -> Result<(),> {
+    fn default() -> Self {
+        Self { mode: Mode::Normal }
+    }
+    fn run(&mut self) -> Result<()> {
         // let mut size = terminal_size().unwrap();
         let stdin = stdin();
         let mut stdout = stdout().into_raw_mode()?;
-        write!(stdout,
+        write!(
+            stdout,
             "{}{}",
             ratatui::termion::clear::All,
-            ratatui::termion::cursor::Goto(1,1)
-            ).unwrap();
+            ratatui::termion::cursor::Goto(1, 1)
+        )
+        .unwrap();
         stdout.flush().unwrap();
         for k in stdin.keys() {
             let key = k.unwrap();
-            match key {
-                Key::Char('a') => Self::draw_rows(&self)?,
-                // Key::Char('i') => ,
-                // Key::Char('x') => ,
-                // Key::Char('s') => ,
-                // Key::Char('r') => ,
-                // Key::Char('v') => ,
-                Key::Ctrl(c) => match c {
-                    'q' => break, 
+            match self.mode {
+                Mode::Normal => match key {
+                    // Key::Char('a') => ,
+                    Key::Char('i') => self.set_mode(Mode::Insert),
+                    // Key::Char('x') => ,
+                    // Key::Char('s') => ,
+                    // Key::Char('r') => ,
+                    // Key::Char('v') => ,
+                    Key::Ctrl(c) => match c {
+                        'q' => break,
+                        _ => {}
+                    },
                     _ => {}
-                }
-                _ => {}
+                },
+                Mode::Insert => match key {
+                    Key::Char(c) => {
+                        write!(stdout, "{}", c)?;
+                    }
+                    Key::Backspace => {
+                        write!(stdout, "\x08 \x08")?;
+                    }
+                    Key::Esc => {
+                        self.set_mode(Mode::Normal);
+                    }
+                    _ => {}
+                },
             }
             stdout.flush().unwrap();
         }
         Ok(())
     }
-    fn draw_rows(&self) -> Result<(),> {
-        let height = terminal_size()?.1;
-        for current_row in 0..height {
-            print!("~");
-            if current_row + 1 < height {
-                print!("\r\n")
-            }
-        }
-        Ok(())
+    fn set_mode(&mut self, mode: Mode) {
+        self.mode = mode;
     }
 }
 
-fn main() -> Result<(), > {
+fn main() -> Result<()> {
     Editor::default().run();
     Ok(())
 }
