e61fee8c80235f3e3c6f2c68e728c008addac191
Oghene-Fejiro Edafeadhe <fejiroe@proton.me>
2026-01-10 14:35:41 +0000

    change alternate screen approach after checking docs again

--- src/lib/terminal.rs
+++ src/lib/terminal.rs
@@ -1,17 +1,18 @@
 use ratatui::termion::{
     cursor,
     raw::{IntoRawMode, RawTerminal},
-    screen::{AlternateScreen, IntoAlternateScreen},
+    screen::{ToAlternateScreen},
 };
 use std::io::{Stdout, Write, stdout};
 
 pub struct Terminal {
-    pub stdout: AlternateScreen<RawTerminal<Stdout>>,
+    pub stdout: RawTerminal<Stdout>,
 }
 
 impl Terminal {
     pub fn new() -> std::io::Result<Self> {
-        let stdout = stdout().into_raw_mode()?.into_alternate_screen()?;
+        let mut stdout = stdout().into_raw_mode()?;
+        write!(stdout, "{}", ToAlternateScreen)?;
         Ok(Self { stdout })
     }
 }
